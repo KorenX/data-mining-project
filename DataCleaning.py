7b4abfd5c253bb4aabf1b9593f59d99b56aac7a3
KorenX <korenxking@gmail.com>
2023-04-22 18:14:30 +0300

    Remove ID3 from the code

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,7 +1,7 @@
 import pandas
 import math
-import ID3
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
 
 MISSING_THRESHOLD = 5
 MIN_MAX_OFFSET = 1
@@ -9,9 +9,9 @@ MIN_MAX_OFFSET = 1
 id_col = "id"
 missing_values = ("?",)
 ranges_dict = {"blood pressure" : (50, 180), "blood glucose random": (70, 490), "serum creatinine": (0.4, 10), "sodium" : (104, 163), "potassium": (2.5, 7.6), "hemoglobin": (6, 17.8), "white blood cell count": (2200, 16700), "red blood cell count": (2.1, 6.5)}
-numeric_fields = ("age", "blood pressure", "specific gravity", "albumin", "sugar", "blood glucose random", "serum creatinine", "sodium", "potassium", "hemoglobin", "packed cell volume", "red blood cell count", "white blood cell count")
-categoric_fields = ("red blood cells", "pus cell", "pus cell clumps", "bacteria", "diabetes mellitus", "coronary artery disease", "appetite", "pedal edema", "anemia", "classification")
-positive_values = ("yes", "normal", "present", "good", "ckd")
+numeric_fields = ["age", "blood pressure", "specific gravity", "albumin", "sugar", "blood glucose random", "serum creatinine", "sodium", "potassium", "hemoglobin", "packed cell volume", "red blood cell count", "white blood cell count"]
+categoric_fields = ["red blood cells", "pus cell", "pus cell clumps", "bacteria", "diabetes mellitus", "coronary artery disease", "appetite", "pedal edema", "anemia", "classification"]
+positive_values = ["yes", "normal", "present", "good", "ckd"]
 
 def remove_out_of_range(db : pandas.DataFrame) -> pandas.DataFrame:
     for field, range in ranges_dict.items():
@@ -46,32 +46,23 @@ def convert_categories_to_binary(db: pandas.DataFrame) -> pandas.DataFrame:
     return db
 
 def fill_in_binary_missing_info(db: pandas.DataFrame) -> pandas.DataFrame:
-    ID3.MAX_LAYERS = 2
     for field in categoric_fields:
-
         col = db[(db[field] == '?')]
         if (len(col) > 0):
             db = _fill_in_col(db, field)
     return db
     
 def _fill_in_col(db: pandas.DataFrame, replace_col: str) -> pandas.DataFrame:
-    learn_db = db.drop([id_col], axis=1)
+    remove_lines = categoric_fields + [id_col]
+    remove_lines.remove(replace_col)
+    learn_db = db.drop(remove_lines, axis=1)
     learn_db = learn_db[learn_db[replace_col] != '?']
-    learn_db = do_binning(learn_db)
-    table = ID3.generate_decision_tree(learn_db, replace_col, ID3.gain_function)
-    missing_db = db[db[replace_col] == '?']
-    for id in missing_db[id_col]:
-        db = _fill_by_id(db, table, id, replace_col)
-    return db
 
-def _fill_by_id(db: pandas.DataFrame, replace_table : dict, id: int, replace_col : str) -> pandas.DataFrame:
-    while True:
-        if type(replace_table) is dict:
-            for field, interval in replace_table:
-                if db.loc[(db[id_col] == id), field].iloc[0] in interval:
-                    replace_table = replace_table[(field, interval)]
-                    break
-        else:
-            db.loc[(db[id_col] == id), replace_col] = replace_table
-            break
+    tree_model = DecisionTreeClassifier(criterion="entropy", max_depth=3)
+    tree_model.fit(learn_db.drop(replace_col, axis=1), learn_db[replace_col])
+
+    fill_db = db[db[replace_col] == '?']
+    fill_db = fill_db.drop(remove_lines + [replace_col], axis=1)
+    filled_col = tree_model.predict(fill_db)
+    db.loc[db[replace_col] == '?', replace_col] = filled_col
     return db
