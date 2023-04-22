a43e304bb1839d52162be27a44942ca89c6c4950
KorenX <korenxking@gmail.com>
2023-04-22 19:53:16 +0300

    Add AdaBoost tree

--- TreeGeneration.py
+++ TreeGeneration.py
@@ -2,7 +2,7 @@ import pandas
 import DataCleaning
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score
 import graphviz
 
@@ -16,29 +16,8 @@ def clean_db(db_name: str, db_new_name: str):
     
     db.to_excel(db_new_name, index=False)
 
-def train_models(db_name: str):
-    db = pandas.read_excel(db_name,  index_col=False).drop("id", axis=1)
-    
-    features = db.drop("classification", axis=1)
-    target = db["classification"]
-
-    f_train, f_test, t_train, t_test = train_test_split(features, target, test_size=0.33, random_state=42)
-
-#GINI DECISION TREE
-    tree_model = DecisionTreeClassifier()
-    tree_model.fit(f_train, t_train)
-
-    graph_data = export_graphviz(tree_model, out_file=None, feature_names=f_train.columns, class_names=['0', '1'], filled=True)
-    graph = graphviz.Source(graph_data)
-    graph.format = "png"
-    graph.render('decision_tree_gini', view=False)
-
-    t_prediction = tree_model.predict(f_test)
-    accuracy = accuracy_score(t_test, t_prediction)
-    percision = precision_score(t_test, t_prediction)
-    print(f"gini scores: {accuracy} {percision}")
-
 #GAIN DECISION TREE
+def gain_tree(f_train, f_test, t_train, t_test):
     tree_model = DecisionTreeClassifier(criterion="entropy", max_depth=None)
     tree_model.fit(f_train, t_train)
 
@@ -52,7 +31,40 @@ def train_models(db_name: str):
     percision = precision_score(t_test, t_prediction)
     print(f"gain scores: {accuracy} {percision}")
 
-#RANDOM FOREST
+#GINI DECISION TREE
+def gini_tree(f_train, f_test, t_train, t_test):
+    tree_model = DecisionTreeClassifier()
+    tree_model.fit(f_train, t_train)
+
+    graph_data = export_graphviz(tree_model, out_file=None, feature_names=f_train.columns, class_names=['0', '1'], filled=True)
+    graph = graphviz.Source(graph_data)
+    graph.format = "png"
+    graph.render('decision_tree_gini', view=False)
+
+    t_prediction = tree_model.predict(f_test)
+    accuracy = accuracy_score(t_test, t_prediction)
+    percision = precision_score(t_test, t_prediction)
+    print(f"gini scores: {accuracy} {percision}")
+
+#ADABOOST DECISION TREE
+def adaboost_tree(f_train, f_test, t_train, t_test):
+    tree_model = DecisionTreeClassifier()
+
+    ada_model = AdaBoostClassifier(base_estimator=tree_model)
+    ada_model.fit(f_train, t_train)
+
+    graph_data = export_graphviz(ada_model.estimators_[0], out_file=None, feature_names=f_train.columns, class_names=['0', '1'], filled=True)
+    graph = graphviz.Source(graph_data)
+    graph.format = "png"
+    graph.render('decision_tree_adaboost', view=False)
+
+    t_prediction = ada_model.predict(f_test)
+    accuracy = accuracy_score(t_test, t_prediction)
+    percision = precision_score(t_test, t_prediction)
+    print(f"adaboost scores: {accuracy} {percision}")
+
+#RANDOM DECISION FOREST
+def random_forest(f_train, f_test, t_train, t_test):
     forest_model = RandomForestClassifier(criterion="entropy", max_depth=None, n_estimators=10)
     forest_model.fit(f_train, t_train)
 
@@ -66,6 +78,18 @@ def train_models(db_name: str):
     percision = precision_score(t_test, t_prediction)
     print(f"forest scores: {accuracy} {percision}")
 
+def train_models(db_name: str):
+    db = pandas.read_excel(db_name,  index_col=False).drop("id", axis=1)
+    
+    features = db.drop("classification", axis=1)
+    target = db["classification"]
+
+    f_train, f_test, t_train, t_test = train_test_split(features, target, test_size=0.33, random_state=42)
+    
+    # gain_tree(f_train, f_test, t_train, t_test)
+    # gini_tree(f_train, f_test, t_train, t_test)
+    adaboost_tree(f_train, f_test, t_train, t_test)
+    random_forest(f_train, f_test, t_train, t_test)
 
 
 if __name__ == "__main__":
